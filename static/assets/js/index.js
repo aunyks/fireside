8a9a1a092eb00017484c1b8f3536a6e098d9cf6b
Gerald Nash <g.nash.dev@gmail.com>
2016-05-09 15:20:52 -0700

    Greatly editing UI

---
+++ static/assets/js/index.js
@@ -0,0 +1,26 @@
+function generateItem(title, picture, description, origin, link){
+    // Create item
+    var item = $('<article></article>');
+    // Create link wrapper
+    var link = $('<a></a>');
+    // Create actual image object
+    var img = $('img');
+    // Set img source
+    
+    // Wrap image in link
+    link.append(img);
+    
+    // Create title
+    var titleTxt = $('<h3></h3>');
+    // Set titleTxt a title
+    
+    // Create description
+    var descTxt = $('<p></p>');
+}
+
+$(document).ready(function(){
+    
+    // Get server info
+    
+    // Create content items
+});
